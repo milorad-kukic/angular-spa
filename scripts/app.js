d457fd9183de6dea43a483de2065099c5718fc9e
Milorad Kukic <kukic.milorad@gmail.com>
2016-10-30 16:02:13 +0100

    Implemented main functionality.

--- scripts/app.js
+++ scripts/app.js
@@ -7,5 +7,33 @@ angular.module('LunchCheck', [])
 
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope) { 
+        $scope.dishes = "";
+        $scope.tooMuch = false;
+        $scope.message = "";
+
+        $scope.checkIfTooMuch = function() {
+            var t = $scope.dishes.split(',');
+            var numberOfItems = 0;
+            
+            if ($scope.dishes.trim() === "") {
+                $scope.message = "Please enter data first";
+                return;
+            } else {
+                for (var i=0; i < t.length; i++) {
+                    var dish = t[i];
+                    if (dish.trim() != "") {
+                        numberOfItems += 1;
+                    }
+                }
+            }
+            console.log(numberOfItems);
+
+
+            if (numberOfItems > 3) {
+                $scope.message = "Too much!";
+            } else {
+                $scope.message = "Enjoy!";
+            }
+        };
     };
 })();
