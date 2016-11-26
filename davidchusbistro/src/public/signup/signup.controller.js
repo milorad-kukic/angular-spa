90a7d0f0aeecf17592f42628b5743ed85a04aa3c
Milorad Kukic <kukic.milorad@gmail.com>
2016-11-26 20:55:03 +0100

    Update searching of favorite dish.

--- davidchusbistro/src/public/signup/signup.controller.js
+++ davidchusbistro/src/public/signup/signup.controller.js
@@ -2,10 +2,23 @@
 'use strict'
 
 angular.module('public')
-    .controller('SignUpController', SignUpController);
+    .controller('SignUpController', SignUpController)
+    .factory('debounce', ['$timeout', function($timeout) {
+        return function(callback, interval) {
+            var timeout = null;
+            return function() {
+                $timeout.cancel(timeout);
+                timeout = $timeout(function() {
+                    var args = Array.prototype.slice.call(arguments);
+                    callback.apply(this, arguments);
+                }, interval);
+            }
+        }
+    }])
+;
 
-SignUpController.$inject = ['$scope', 'MenuService', 'UserService'];
-function SignUpController($scope, MenuService, UserService) {
+SignUpController.$inject = ['$scope', 'MenuService', 'UserService', 'debounce'];
+function SignUpController($scope, MenuService, UserService, debounce) {
     var signupCtrl = this;
 
     signupCtrl.favoriteDishObj = null;
@@ -25,14 +38,16 @@ function SignUpController($scope, MenuService, UserService) {
         return UserService.getSignedUser();
     }
 
-    $scope.$watch("signupCtrl.favoriteDish", function(newValue, oldValue) {
-        if (newValue !== oldValue) {
-            var p = MenuService.getMenuItem(newValue);
+    $scope.$watch("signupCtrl.favoriteDish", debounce(function() {
+        if(!signupCtrl.favoriteDish){
+            return;
+        }
+            var p = MenuService.getMenuItem(signupCtrl.favoriteDish);
             p.then(function(data) {
                 signupCtrl.favoriteDishObj = data;
             });
-        }
-    });
+        }, 800)
+    )
 };
 
 })();
